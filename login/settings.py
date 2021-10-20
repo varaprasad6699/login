54e03b7565f4a2518b99536ce0c2d9ab70964edd
varaprasad6699 <73052233+varaprasad6699@users.noreply.github.com>
2021-10-20 14:04:13 +0530

    Update settings.py

--- login/settings.py
+++ login/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%5rn-^o_@eo*ru0+yf5ho15+j)rgav#ere*)k0u#)s*3_!lwnm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','logpage.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','logpage.herokuapp.com']
 
 
 # Application definition
@@ -129,3 +129,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
